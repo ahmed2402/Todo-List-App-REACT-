469e4be2fada853ceeb804fd3ebfeeaff668bad0
ahmed2402 <ahmedraza312682@gmail.com>
2025-01-05 23:20:20 +0500

    Final Touches

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -77,24 +77,24 @@ function App() {
     <>
       <Navbar/>
       <div className=" mx-3 md:container  md:mx-auto my-5 rounded-xl bg-violet-100 p-5 min-h-[80vh] md:w-[35%]">
-      <h1 className='font-bold text-center text-3xl'>iTask - Manage your todos at one place</h1>
+      <h1 className='font-bold text-center text-3xl'>Listify - Simplify your tasks, amplify your life</h1>
          <div className="addtodo my-5 flex flex-col gap-4">
-        <h2 className='text-2xl font-bold'>Add todo</h2> 
+        <h2 className='text-2xl font-bold'>Create a Task</h2> 
         <div className="flex">
-        <input onChange={handleChange} value={todo} type="text" className='w-full rounded-full px-5 py-1' />
+        <input onChange={handleChange} value={todo} type="text" className=' font w-full rounded-full px-5 py-1' />
       <button onClick={handleAdd} disabled={todo.length<=0} className='bg-violet-800 hover:bg-violet-950 disabled:bg-violet-500 rounded-full mx-2 py-2 p-4 text-white text-sm font-bold'>Save</button>
       </div>
       </div>
       <input className='my-4' id='show' onChange={toggleFinished} type="checkbox" checked={showFinished} /> 
-         <label className='mx-2' htmlFor="show">Show Finished</label> 
+         <label className=' font mx-2' htmlFor="show">Show Finished</label> 
          <div className='h-[1px] bg-black opacity-15 w-[90%] mx-auto my-2'></div>
      
-                <h2 className='text-2xl font-bold'>Your Todos</h2>
+                <h2 className='text-2xl font-bold'>Your Tasks</h2>
                 <div className="todos">
-                  {todos.length === 0 && <div className='m-5 text-xl font-bold'>No todos to display</div>}
+                  {todos.length === 0 && <div className='m-5 text-xl font-bold'>All clear! Add a new task to stay productive.</div>}
                   {todos.map(item =>{
 
-         return (showFinished || !item.iscompleted) && <div key={item.id} className={"todo flex my-3 justify-between "}>
+         return (showFinished || !item.iscompleted) && <div key={item.id} className={"font text-xl todo flex my-3 justify-between "}>
           <div className='flex gap-5'>
           <input onChange={handleCheckbox} name={item.id} type="checkbox" checked={item.iscompleted}  id="" />
           <div className={item.iscompleted?"line-through":""}>{item.todo}</div>
--- todo-app/src/components/Navbar.jsx
+++ todo-app/src/components/Navbar.jsx
@@ -4,11 +4,11 @@ const Navbar = () => {
   return (
     <nav className='flex justify-around bg-indigo-900 text-white py-2'>
         <div className="logo">
-            <span className='font-bold text-xl mx-8'>ITask</span>
+            <span className='font-bold text-xl mx-8'>Listify</span>
         </div>
         <ul className='flex gap-8 mx-9 '>
-           <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-           <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
+           <li className='cursor-pointer font-semibold hover:font-bold transition-all'>Home</li>
+           <li className='cursor-pointer font-semibold hover:font-bold transition-all'>Your Tasks</li>
         </ul>
    
     </nav>
